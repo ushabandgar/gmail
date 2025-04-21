cbc5eb32db71d8bfafb6cd923178f2fdeaf0d624
ushabandgar <ubandgar@birdzi.com>
2025-04-21 10:41:47 +0530

    test cases on 21 april

--- src/main/java/com/gmail/stepdefinations/CreateAccountSteps.java
+++ src/main/java/com/gmail/stepdefinations/CreateAccountSteps.java
@@ -3,19 +3,32 @@ package com.gmail.stepdefinations;
 import com.gmail.pages.CreateAccountPage;
 
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class CreateAccountSteps {
 
-	 
 	@Then("{string} text is displayed correctly to users")
-	public void createAccountDisplayedOrNot(String CreateAccount) {
-		CreateAccountPage createAccountPage=new CreateAccountPage();
-		createAccountPage.verifyCreateAccountDisplayed(CreateAccount);
+	public void createAccountDisplayedOrNot(String createAccount) {
+		CreateAccountPage createAccountPage = new CreateAccountPage();
+		createAccountPage.verifyCreateAccountDisplayed(createAccount);
 	}
 
 	@Then("{string} button is clickable")
-	public void reateAccountClickableOrNot(String CreateAccount) {
-		CreateAccountPage createAccountPage=new CreateAccountPage();
-		createAccountPage.verifyCreateAccountClickable(CreateAccount);
+	public void createAccountClickableOrNot(String createAccount) {
+		CreateAccountPage createAccountPage = new CreateAccountPage();
+		createAccountPage.verifyCreateAccountClickable(createAccount);
+	}
+
+	@When("User click on {string} button")
+	public void clickOnCreateAccount(String CreateAccount) {
+		CreateAccountPage createAccountPage = new CreateAccountPage();
+		createAccountPage.clickOnCreateAccount(CreateAccount);
+	}
+	
+	
+	@Then("On click of {string} showing options {string} and {string}")
+	public void createAccountClickOptions(String createAccount,String forMyPersonalUse, String forMyWorkOrBusiness) {
+		CreateAccountPage createAccountPage = new CreateAccountPage();
+		createAccountPage.verifyCreateAccountOptions(createAccount,forMyPersonalUse,forMyWorkOrBusiness);
 	}
 }
--- target/test-classes/Features/CreateAccount.feature
+++
@@ -1,9 +0,0 @@
-Feature: Create Account functionality
-
-Scenario: Verify that the "Create account" text is displayed correctly to users.
-Then "Create account" text is displayed correctly to users
-
-Scenario: Verify that the "Create account" button is clickable
-Then "Create account" button is clickable
-
-
